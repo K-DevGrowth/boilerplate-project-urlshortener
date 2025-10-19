4ec66e4016b7b5afa885f7c13971408056b456ef
Khoi <khoidang01234@gmail.com>
2025-10-19 20:24:20 +0700

    delete module isn't used

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const dns = require("dns");
-const urlParser = require("url");
 
 const app = express();
 const port = process.env.PORT || 3000;
